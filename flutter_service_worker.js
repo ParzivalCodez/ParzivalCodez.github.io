728a0c88d4a80d0d73cd46f1d9e01f7e2321c1b7
ParzivalCodez <htmlhectorcodez@gmail.com>
2024-12-26 23:13:51 -0800

    Updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,10 +24,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_st.js.symbols": "327a3060925e525407f4f2747a4712d6",
 "canvaskit/skwasm_st.wasm": "809674c831d83f7f9c71d9dd93771403",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "743bda053ffcaa810a5dcf378f94e2b3",
+"flutter_bootstrap.js": "91c18ac5532d5cb32be23ffddbc4e830",
 "index.html": "08402fcab4d430597e999ad0a05c17c4",
 "/": "08402fcab4d430597e999ad0a05c17c4",
-"main.dart.js": "606c8e31b1ca7214ae5b54a482feaf79",
+"main.dart.js": "af7cff3f64c15cfb7b3fd311c04d54b1",
 "manifest.json": "2a62834eafed927d93ea9f65a44558c6",
 "version.json": "727c86d4959d2fd5bf94deeed7b28da7"};
 // The application shell files that are downloaded before a service worker can
